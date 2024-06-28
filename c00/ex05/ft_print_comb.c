6724655a174fc70fcf8180e0300fd334bf215204
ilorez <zobirnajdaoui@gmail.com>
2024-06-28 01:47:20 +0100

    ex05 and ex06

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 18:21:34 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/06/27 18:37:10 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/06/27 23:26:50 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/06/27 23:46:07 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,40 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_prints_digits(int i, int j, int k)
+{
+	ft_putchar(48 + i);
+	ft_putchar(48 + j);
+	ft_putchar(48 + k);
+	if (i != 7)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_prints_digits(i, j, k);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
 }
--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 18:21:34 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/06/27 18:37:10 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/06/28 00:02:33 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/06/28 01:35:25 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,36 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_print_d(int a, int b, int c, int d)
+{
+	ft_putchar(a + 48);
+	ft_putchar(b + 48);
+	ft_putchar(' ');
+	ft_putchar(c + 48);
+	ft_putchar(d + 48);
+	if (!(c == 9 && d == 9 && a == 9 && b == 8))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_print_d(i / 10, i % 10, j / 10, j % 10);
+			j++;
+		}
+		i++;
+	}
 }
