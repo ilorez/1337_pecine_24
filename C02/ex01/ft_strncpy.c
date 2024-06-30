33752ee8f8d746b43bf2bad055f3c430859aa96a
ilorez <zobirnajdaoui@gmail.com>
2024-06-30 11:11:19 +0100

    ex00 and ex01

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 09:01:22 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/06/30 09:47:25 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while(src[i] != '\0'  && i <= n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int main()
+{
+	char	str[] = "najdaoui";
+	char	des[4];
+
+	ft_strncpy( des, str, 4);
+
+	printf("%s\n", des);
+}
