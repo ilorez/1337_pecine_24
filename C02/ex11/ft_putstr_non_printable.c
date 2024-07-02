950dc0977c028a46dc029887afa165a40a5ecd90
ilorez <zobirnajdaoui@gmail.com>
2024-07-02 09:08:54 +0100

    done with C02 test still just push it

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,68 +6,55 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:20:26 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/01 22:17:05 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/02 09:05:29 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	len;
+
+	len = 0;
+	if (size > 0)
+	{
+		while (src[len] && --size)
+		{
+			dest[len] = src[len];
+			len++;
+		}
+		dest[len] = '\0';
+	}
+	while (src[len])
+	{
+		len++;
+	}
+	return (len);
+}
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		ft_putchar(str[i++]);
-}
-
-void	ft_dec_to_hexa(int n, char *dest)
-{
-	int	i;
-	int	teamp;
-
-	i = 0;
-	if (n == 0)
-	{
-		dest[i] = '0';
-		dest[i + 1] = '0';
-	}
-	while (n != 0)
-	{
-		if (i == 0 && n < 16)
-			dest[i++] = 48;
-		teamp = n % 16;
-		if (teamp < 10)
-			dest[i] = teamp + '0';
-		else
-			dest[i] = teamp + 'a' - 10;
-		i++;
-		n = n / 16;
-	}
-}
-
 void	ft_putstr_non_printable(char *str)
 {
-	char			hex[100];
 	unsigned int	i;
+	char			hex_str[17];
 
+	ft_strlcpy(hex_str, "0123456789abcdef", 17);
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] < 127 && str[i] >= ' ')
+		if (str[i] < 127 && str[i] > 31)
 		{
 			ft_putchar(str[i]);
 		}
 		else
 		{
 			ft_putchar('\\');
-			ft_dec_to_hexa(str[i], hex);
-			ft_putstr(hex);
+			ft_putchar(hex_str[(unsigned char)str[i] / 16]);
+			ft_putchar(hex_str[(unsigned char)str[i] % 16]);
 		}
 		i++;
 	}
