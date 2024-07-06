b4778dce303cffe6b29330eb17e69c9be93a7c99
ilorez <zobirnajdaoui@gmail.com>
2024-07-06 08:36:19 +0100

    edit ft_strlcat

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:00:07 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/03 10:25:22 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/06 08:35:54 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 	return (i);
 }
 
@@ -33,12 +31,9 @@ void	ft_strncat(char *dest, char *src, unsigned int nb, unsigned int dl)
 		dl++;
 		j++;
 	}
-	while (j < nb)
-	{
-		dest[dl] = '\0';
-		dl++;
-		j++;
-	}
+	dest[dl] = 0;
+	while (j++ < nb)
+		dest[++dl] = 0;
 }
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
