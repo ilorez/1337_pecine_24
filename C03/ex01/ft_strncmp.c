2a336b6529eee652af70b5e10e18bd6c533d0f78
ilorez <zobirnajdaoui@gmail.com>
2024-07-06 11:15:42 +0100

    norminette

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,18 +6,18 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 12:19:13 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/06 11:08:54 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/06 11:14:49 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
-	
+
 	i = 0;
 	if (n == 0)
 		return (0);
-	while ((s2[i] || s1[i])  && i < n)
+	while ((s2[i] || s1[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 		{
@@ -25,6 +25,5 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		}
 		i++;
 	}
-
 	return (0);
 }
