fe5e2fd7a2bca5488daea42db5a0bde76aa79280
ilorez <zobirnajdaoui@gmail.com>
2024-07-06 07:52:51 +0100

    ex03

---
+++ C05/ex03/ft_sort_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 13:20:31 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/04 13:23:07 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_sort_array(
+
+int	main(int argc, char *argv[])
+{
+	while (--argc)
+	{
+		ft_putstr(argv[argc]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
