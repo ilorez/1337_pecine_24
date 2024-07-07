ca8781c4484e0f5b4c2c92ed263e47d1e804fd6f
ilorez <zobirnajdaoui@gmail.com>
2024-07-07 14:22:06 +0100

    strlcat you should return the size + srclen not the dest len + src len in case of size <= dest_len

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:00:07 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/06 16:26:20 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/07 14:21:05 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,20 @@ void	ft_strncat(char *dest, char *src, unsigned int nb, unsigned int dl)
 		dest[dl + j] = src[j];
 		j++;
 	}
-	dest[dl + j] = 0;
-	while (++j < nb)
-		dest[dl + j] = 0;
+	dest[dl + j] = '\0';
 }
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	dest_p_src_len;
 	unsigned int	dest_len;
+	unsigned int	src_len;
 
+	src_len = ft_strlen(src);
 	dest_len = ft_strlen(dest);
-	dest_p_src_len = dest_len + ft_strlen(src);
-	if (size == 0 || size <= dest_len)
-		return (dest_p_src_len);
+	dest_p_src_len = dest_len + src_len;
+	if (size <= dest_len)
+		return (size + src_len);
 	ft_strncat(dest, src, (size - dest_len - 1), dest_len);
 	return (dest_p_src_len);
 }
