d7f21bb223370f32336c093c4e784132ce405122
ilorez <zobirnajdaoui@gmail.com>
2024-07-07 21:13:06 +0100

    ex07

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 20:15:47 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/07 20:24:59 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	_ft_is_prime(int nb, int i)
+{
+	if (i == nb)
+		return (1);
+	if (nb % i == 0)
+		return (0);
+	_ft_is_prime(nb, i + 1);
+}
+
+int	ft_is_prime(int nb)
+{
+	return (_ft_is_prime(nb, 2));
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	if (ft_is_prime(nb))
+		return (nb);
+	ft_find_next_prime(++nb);
+}
