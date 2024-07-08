1d3cb20a02e750bf89f3d8724d22d167b5128ff1
ilorez <zobirnajdaoui@gmail.com>
2024-07-08 11:22:29 +0100

    ft_sort params

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 13:20:31 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/04 13:23:07 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/08 11:15:22 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,74 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	ft_sort_array(
-
-int	main(int argc, char *argv[])
+int	ft_strcmp(char *s1, char *s2)
 {
-	while (--argc)
+	int	i;
+
+	i = 0;
+	while (s2[i] && s1[i] == s2[i])
 	{
-		ft_putstr(argv[argc]);
-		ft_putchar('\n');
+		i++;
 	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_print_strs(char *strs[], int *sorted_indexs, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = 0;
+		while (j < len)
+		{
+			if (sorted_indexs[j] == i)
+			{
+				ft_putstr(strs[j + 1]);
+				ft_putchar('\n');
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(const int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	int	sorted_indexs[argc - 1];
+	int	pos;
+
+	i = 0;
+	while (++i < argc)
+	{
+		j = 0;
+		pos = 0;
+		while (++j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				pos++;
+		}
+		sorted_indexs[i - 1] = pos;
+	}
+	ft_print_strs(argv, sorted_indexs, argc - 1);
 	return (0);
 }
+// test 1
+/*
+				ft_putchar(48+j);
+				ft_putchar(':');
+				ft_putchar(48+j+1);
+*/
+// test 2
+/*
+		ft_putchar(i+48);
+		ft_putchar(':');
+		ft_putchar(pos + 48);
+		ft_putchar('\n');
+*/
+
+// write(1, "----------\n", 11);
