0ac23e0647d022a76509feec9d70b687d79ef8a8
ilorez <zobirnajdaoui@gmail.com>
2024-07-08 15:29:22 +0100

    fix atoi you should stop after find a (-) or (+) followed by not a number

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 11:51:34 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/06 17:16:09 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:26:23 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@ int	is_number(char c)
 	return (0);
 }
 
-int	ft_is_space(char c)
+int	ft_get_my_str(char **str, int *signe)
 {
-	if (c == '\t' || c == '\n' || c == '\v')
-		return (1);
-	else if (c == '\f' || c == '\r' || c == 32)
-		return (1);
-	return (0);
-}
+	int	i;
 
-int	ft_is_num_space_p_m(char c)
-{
-	if (ft_is_space(c) || is_number(c) || c == '+' || c == '-')
-		return (1);
-	return (0);
+	i = 0;
+	while (**str && ((**str >= 9 && **str <= 13) || **str == 32))
+		(*str)++;
+	while (**str == '+' || **str == '-')
+	{
+		if (**str == '-')
+			*signe *= -1;
+		(*str)++;
+	}
+	while (is_number((*str)[i]))
+		i++;
+	return (i);
 }
 
 int	ft_atoi(char *str)
@@ -38,21 +40,22 @@ int	ft_atoi(char *str)
 	int	i;
 	int	signe;
 	int	my_n;
+	int	len;
 
 	my_n = 0;
 	i = 0;
 	signe = 1;
-	while (str[i] && ft_is_num_space_p_m(str[i]))
+	len = ft_get_my_str(&str, &signe);
+	while (i < len)
 	{
-		if (is_number(str[i]))
-		{
-			my_n = my_n * 10 + (str[i] - 48);
-			if (str[i + 1] && (str[i + 1] < '0' || str[i + 1] > '9'))
-				return (my_n * signe);
-		}
-		if (str[i] == '-')
-			signe *= -1;
+		if (!is_number(str[i]))
+			break ;
+		my_n = my_n * 10 + (str[i] - 48);
 		i++;
 	}
-	return (my_n * signe);
+	return (my_n * (signe));
 }
+/*
+int main() {
+    printf("%d\n", ft_atoi("  +-+----22"));
+}*/
