ed5c81675c8fcc7a79c3e923e2d9b626812a6afd
ilorez <zobirnajdaoui@gmail.com>
2024-07-08 18:35:21 +0100

    ex01

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -1,50 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 16:20:15 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/08 17:11:31 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/07/08 17:20:28 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/08 17:28:36 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	*ft_range(int min, int max)
 {
+	int	arr;
 	int	i;
 
 	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strdup(char *src)
-{
-	char	*dest;
-
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
-	if (dest)
-	{
-		ft_strcpy(dest, src);
-		return (dest);
-	}
-	return (0);
+	arr = (int *)malloc(sizeof(int) * (max - min));
+	if (!arr)
+		return (0);
+	while (min < max)
+		arr[i++] = min++;
+	return (arr);
 }
