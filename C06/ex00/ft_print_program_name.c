7abe924f617b162ed9613833da2049ff3083412c
ilorez <zobirnajdaoui@gmail.com>
2024-07-08 11:52:18 +0100

    hihi just for no error because i didn't use the argc param

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 11:51:20 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/04 11:57:13 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/08 11:49:15 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -30,7 +30,7 @@ void	ft_putstr(char *str)
 
 int	main(int argc, char *argv[])
 {
-	ft_putstr(argv[0]);
+	ft_putstr(argv[argc * 0]);
 	ft_putchar('\n');
 	return (0);
 }
