422d8e964b5c252a465bb5a0041eff566bb9ca40
ilorez <zobirnajdaoui@gmail.com>
2024-07-10 11:54:20 +0100

    i think i have done with this one

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,64 +6,106 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:36:37 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/09 22:19:12 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/10 11:53:43 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi_base(char *str, char *base);
+int		ft_atoi_base(char *str, char *base);
+int		ft_is_valid_base(char *base);
 
-int	ft_number_digits(int	nbr)
+int	ft_get_b_size(int nbr, int len)
 {
-	if (nbr < 10)
+	if (nbr < len)
 		return (1);
-	return (1 + ft_number_digits(nbr / 10));
+	return (1 + ft_get_b_size(nbr / len, len));
 }
 
-void	_ft_putnbr_base(int nbr, char *base, int len)
+int	ft_strlen(char *str)
 {
-	int	mod;
+	int	i;
 
-	if (nbr > 0)
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_rev_str(char *str, int len)
+{
+	int		i;
+	char	swap;
+
+	i = 0;
+	if ((str)[0] == '-')
 	{
-		mod = nbr % len;
-		nbr /= len;
-		_ft_putnbr_base(nbr, base, len);
-		// use maloc to store this numb as string
-		//ft_putchar(base[mod]);
+		i = 1;
+		len++;
+	}
+	while (i <= (len / 2))
+	{
+		swap = (str)[i];
+		(str)[i] = (str)[len - i];
+		(str)[len - i] = swap;
+		i++;
 	}
 }
 
-void	ft_putnb_base(int nbr, char *base)
+// l47: printf("%s\n-----\ni = %d\n-----\nlen = %d\n----\n", str, i, len);
+void	ft_putnb_base(char **result, int nbr, char *base, int m_s)
 {
+	int	i;
 	int	bl;
 
-	bl = ft_strlen(base);
-	if (bl <= 1 || !is_valid_base(base))
-		return ;
-	else if (nbr < 0)
+	i = 0;
+	if (nbr < 0)
 	{
+		(*result)[i++] = '-';
 		nbr = -nbr;
-		// store mines
 	}
-	if (nbr == 0)
-	// store 0
-	else
-		_ft_putnbr_base(nbr, base, bl);
+	bl = ft_strlen(base);
+	while (i < m_s)
+	{
+		(*result)[i] = base[nbr % bl];
+		nbr /= bl;
+		i++;
+	}
+	ft_rev_str(&(**result), m_s - 1);
 }
 
 char	*ft_convert_base(char *nbr, char *base_form, char *base_to)
 {
-	int	decnum;
+	int		decnum;
+	int		m_s;
+	char	*result;
+	int		btl;
 
-	decum = ft_atoi_base(nbr, base_form);
-
-
-	// convert from (nbr in base form ) to nbm in (nbr int base 10) using atoi base
-	// from the another file ft_convert_base2.c
-
-	// after get the number convert it to (base to) by editing in putnbr base funtioin
-	
-   	// return the nbr	
+	if (!ft_is_valid_base(base_to))
+	{
+		printf("not valid");
+		return (0);
+	}
+	decnum = ft_atoi_base(nbr, base_form);
+	btl = ft_strlen(base_to);
+	if (decnum < 0)
+		m_s = 1 + ft_get_b_size(-decnum, btl);
+	else
+		m_s = ft_get_b_size(decnum, btl);
+	result = (char *)malloc(sizeof(char) * (m_s + 1));
+	if (!result)
+		return (NULL);
+	result[m_s - 1] = 0;
+	if (decnum == 0)
+		(result)[0] = '0';
+	else
+		ft_putnb_base(&result, decnum, base_to, m_s);
+	return (result);
 }
+
+/*int	main(int argc, char **argv)
+{
+	printf("%s\n", ft_convert_base(argv[argc * 0 + 1], argv[2], argv[3]));
+	return (0);
+}*/
