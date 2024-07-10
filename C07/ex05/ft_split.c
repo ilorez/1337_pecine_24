b692ad23683fa4f99e0b7c8b4e40b4d206c5bb0c
ilorez <zobirnajdaoui@gmail.com>
2024-07-10 19:14:00 +0100

    some changes

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,66 +6,87 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 12:01:40 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/10 17:01:57 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/10 18:21:10 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_is_sep(char c, char *seps)
+int    ft_is_sep(char c, char *seps)
 {
-	int	i;
+    int    i;
 
-	while(seps[i])
-		if (seps[i++] == c)
-			return (0);
-	return (1);
+    i = 0;
+    while(seps[i])
+        if (seps[i++] == c)
+            return (1);
+    return (0);
 }
 
-int	ft_count_words(char *str,char seps)
+int    ft_count_words(char *str,char *seps)
 {
-	int	i;
-	int	w;
+    int    i;
+    int    w;
 
-	w = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (ft_is_sep(str[i]))
-			if (i > 0 && !ft_is_sep(str[i - 1])
-					w++;
-		i++;
-	}
-	return (w);
+    w = 0;
+    i = 0;
+    while (str[i])
+    {
+        if (ft_is_sep(str[i], seps))
+            if (i > 0 && !ft_is_sep(str[i - 1], seps))
+                    w++;
+        i++;
+    }
+    if (!ft_is_sep(str[i-1], seps))
+        w++;
+    return (w);
 }
 
-char	**ft_split(char	*str, char *charset)
+char    **ft_split(char    *str, char *charset)
 {
-	char	**strings;
-	char	*start;
-	int	i;
-	char	*word;
-		
-	strings = malloc(sizeof(char) * (ft_count_worlds(str, charset)));
-	i = 0;
-	start = str;
-	while (*str)
-	{
-		if (*(str+1) && str == start && ft_is_sep(*str, charset))
-			start++;
-		if (str != start && ft_is_sep(*str, charset))				
-		{
-			//if (!ft_is_sep(*(str-1), charset))
-			//{
-				word = malloc(str - start + 1);
-				strings[i] = word; 
-				while (start != str)
-					(*word)++ = (*start)++; // go from start of word to the position we are in now that count as a word
-				*word = 0; // null terminate the word
-				i++; // go to next word
-			start++;
-		}
-		str++; // go next letter
-	}
-	
+    char    **strings;
+    char    *start;
+    int    i;
+    char    *word;
+    int w;
+
+    w = (ft_count_words(str, charset));
+    strings = malloc(sizeof(char *) * w);
+    i = 0;
+    start = str;
+    while (*str)
+    {
+        if (*(str + 1) && str == start && ft_is_sep(*str, charset))
+            start++;
+        if ((str != start && ft_is_sep(*str, charset)))
+        {
+            //if (!ft_is_sep(*(str-1), charset))
+            //{
+            word = malloc(sizeof(char) * (str - start + 1));
+            strings[i] = word;
+            while (start != str)
+                *word++ = *start++; // go from start of word to the position we are in now that count as a word
+            *word = 0; // null terminate the word
+            i++; // go to next word
+            start++;
+        }
+        str++; // go next letter
+    }
+    return (strings);
+}
+
+int    main()
+{
+    char **strs;
+    int    i;
+
+    strs = ft_split(" o w ", " ");
+    printf("woow");
+    i = 0;
+    while (i < 2)
+    {
+        printf("string %d: %s\n",i ,strs[i++]);
+    }
+    return (0);
 }
