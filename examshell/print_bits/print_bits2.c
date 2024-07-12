3c6713e359f2ba53479b5dd248d72259ecaf9602
ilorez <zobirnajdaoui@gmail.com>
2024-07-12 10:30:50 +0100

    just cool another solution not from me but some one have build it's really understand bits very well

--- examshell/print_bits/print_bits2.c
+++ examshell/print_bits/print_bits2.c
@@ -1,51 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_bits.c                                       :+:      :+:    :+:   */
+/*   print_bits2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 09:23:35 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/12 10:29:24 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/12 10:22:27 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	_print_bits(unsigned char octet, int i)
-{
-	// int mod;
-	if ((i % 8 != 0) && octet == 0)
-	{
-		ft_putchar(48);
-		_print_bits(octet, ++i);
-		return ;
-	}
-	else if ( i % 8 == 0 && octet == 0 && i >= 7)
-	{
-		return ;
-	}
-	//	ft_putchar('-');
-	//	ft_putchar(octet);
-	//	ft_putchar('_');
-	// mod = octet % 2;
-	// octet = octet / 2;
-	_print_bits(octet / 2, ++i);
-	ft_putchar((octet % 2 + 48));
-}
-
 void	print_bits(unsigned char octet)
 {
-	_print_bits(octet, 0);
+	int integer;
+	int dividend;
+
+	dividend = 128;
+	integer = octet;
+	while (dividend != 0)
+	{
+		if (dividend <= integer)
+		{
+			write(1, "1", 1);
+		integer = integer % dividend;
+		}
+		else
+			write(1, "0", 1);
+		dividend = dividend / 2;
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	(void)argv;
-	print_bits(argc * 0);
+	print_bits(argc);
 }
