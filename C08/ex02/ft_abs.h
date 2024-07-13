3011e87f762c4bf3c7d28bd37e3f7739cf3551ab
ilorez <zobirnajdaoui@gmail.com>
2024-07-13 11:49:36 +0100

    C08 to ex04

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -1,21 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/11 14:27:33 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/13 10:41:54 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/07/13 10:58:22 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/13 11:01:01 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef FT_H
-# define FT_H
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-
+#ifndef FT_ABS_H
+# define FT_ABS_H
+# define ABS(N) (NB * (NB >= 0) - NB * (NB < 0))
 #endif
--- C08/ex04/ft_point.h
+++ C08/ex04/ft_point.h
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/11 14:27:33 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/13 10:41:54 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/07/13 11:02:35 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/13 11:37:33 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef FT_H
-# define FT_H
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct t_point
+{
+	int	x;
+	int	y;
+}	t_point;
 
 #endif
