ae149b7540f3870c2d3831834a166b448a26bedf
ilorez <zobirnajdaoui@gmail.com>
2024-07-13 15:40:47 +0100

    fix N -> NB

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -6,11 +6,11 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:58:22 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/13 11:01:01 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/13 15:40:33 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
-# define ABS(N) (NB * (NB >= 0) - NB * (NB < 0))
+# define ABS(NB) (NB * (NB >= 0) - NB * (NB < 0))
 #endif
