dadd86bab7e6aa4155e4becf4d312a72db151877
ilorez <zobirnajdaoui@gmail.com>
2024-07-16 21:31:43 +0100

    i forget the \n

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,17 +6,17 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:29:06 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/07/13 15:15:21 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/07/16 21:30:54 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define EVEN(NBR) NBR % 2 == 0
+# define EVEN(Z) (Z % 2 == 0)
 # define TRUE 1
 # define FALSE 0
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 # include <unistd.h>
 
