16ee47ec41c7548378a63f34eadcf12fee9ba8e3
ilorez <zobirnajdaoui@gmail.com>
2024-10-21 12:07:28 +0100

    some edits

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -6,7 +6,7 @@
 #    By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/16 13:58:31 by znajdaou          #+#    #+#              #
-#    Updated: 2024/07/17 14:52:25 by znajdaou         ###   ########.fr        #
+#    Updated: 2024/07/17 23:00:13 by znajdaou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,5 @@
 rm -f libft.a
 rm -f ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
 cc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
-ar -rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
-ranlib libft.a
+ar -rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
 rm -f ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
