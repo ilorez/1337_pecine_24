f2b64e9df420b004dbb331cfe039455ad529ea1d
ilorez <zobirnajdaoui@gmail.com>
2024-10-21 12:01:58 +0100

    some other files

---
+++ C08/ex05/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 10:11:39 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/07/14 10:13:50 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}			t_stock_str;
+
+#endif // FT_STOCK_STR_H
